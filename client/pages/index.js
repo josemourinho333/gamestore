12e0a38bfb64965bd4ffd7dcc86a354fb24a4304
Derine Banis <d.banis.13@gmail.com>
2022-11-03 11:01:47 -0700

    Get data from igdb. CORS error. Will come back to this

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,43 @@
 import { GameCard } from "../components/GameCard"
+import axios from "axios"
 
 export default function Home() {
+
+  //Affected by CORS - will come back to this.
+
+  // const clientId = process.env.NEXT_PUBLIC_CLIENT_ID
+  // const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET
+  // let accessToken; 
+
+  // const getAccessToken = async() =>{
+  //   const authURL = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`
+  //   await axios.post(authURL)
+  //   .then((response)=>{accessToken = response.data.access_token})
+  // }
+
+  // const getVideoGames = async() => {
+  //   await axios({
+  //     url: "https://api.igdb.com/v4/games",
+  //     method: 'POST',
+  //     headers: {
+  //         'Accept': 'application/json',
+  //         'Client-ID': clientId,
+  //         'Authorization': `Bearer ${accessToken}`,
+  //     },
+  //     data: 'name'
+  //   })
+  //     .then(response => {
+  //         console.log(response.data);
+  //     })
+  //     .catch(err => {
+  //         console.error(err);
+  //     });
+  // }
+
+  // getAccessToken();
+  // getVideoGames();
+
+
   return (
     <>
       <div className="font-bold">
