15e2560189bcdf01119d8f43bfd7f8ba8c6c215d
Derine Banis <d.banis.13@gmail.com>
2022-11-03 14:23:16 -0700

    Restyle game card

--- client/components/GameCard.jsx
+++ client/components/GameCard.jsx
@@ -4,7 +4,7 @@ export const GameCard = (props) => {
 
 
   //Variables for quick change of values in cards
-  const gameName = props.name
+  const gameName = props.name;
   //Game Genres Array. Will be sliced to display the first 2.
   const gameGenres = props.genres;
   const gameImg = props.image;
@@ -17,33 +17,35 @@ export const GameCard = (props) => {
       <figure><img src={gameImg} alt={gameName} /></figure>
       <div className="card-body">
         <div className="game-details flex">
-          <h2 className="card-title text-xl flex flex-wrap w-1/2">
-            {gameName}
+          <div className="title-section">
+            <h2 className="card-title text-xl flex w-fit">
+              {gameName}
+            </h2>
             {/* If the game is on sale somewhere, show this badge */}
-            {isSale && <div className="badge badge-accent">ON SALE</div>}
-            
-          </h2>
+            {isSale && <div className="badge badge-accent ">ON SALE</div>}
+
+          </div>
           {/* Lowest price of game */}
           <p className="price-of-game flex justify-end">{priceOfGame}</p>
         </div>
 
-        <div className="flex">
-          <div className="genre-tags flex-col w-3/5">
-            <p className="text-base">Genres</p>
+        <div className="flex-col">
+          <p className="genre-title text-base">Genres: </p>
+          <div className="genre-section flex flex-wrap">
             {/*Slice genre array to display first two*/}
-            {gameGenres.slice(0,5).map((element, index) => {
+            {gameGenres.slice(0,2).map((element, index) => {
               return (
                 <div key={index} className="badge badge-outline badge-accent w-fit mr-1">{element}</div>
               );
             })}
             {/* A clickable badge to indicate there are more genres. Takes you to game page.  */}
-            {gameGenres.length > 2 && <span className="badge badge-outline badge-accent w-1/4">...</span>}
+            {/* {gameGenres.length > 2 && <span className="badge badge-outline badge-accent w-1/4">...</span>} */}
           </div>
 
-          <div className="card-actions flex-col justify-start items-end w-2/5 ">
-            <button className="btn btn-secondary w-1/2">Buy</button>
-          </div>
         </div>
+          <div className="card-actions">
+            <button className="btn btn-secondary w-full">Buy</button>
+          </div>
       </div>
 
     </div>
